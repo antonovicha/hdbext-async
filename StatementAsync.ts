f74b978d7fe89a47a51b109d1f8c45fb87519493
aleksei antonovich <aleksei.antonovich@gmail.com>
2019-05-26 13:43:04 +0200

    Correcting Statement.exec().

--- StatementAsync.ts
+++ StatementAsync.ts
@@ -2,10 +2,12 @@ import { Statement } from "@sap/hana-client";
 import { promisify } from "util";
 
 class StatementAsync {
-  public exec: <TResults>(params?: any[] | null, options?: {} | null) => Promise<TResults>;
+  public exec: <TResults>(params?: Array<string | number | boolean>, options?: {}) => Promise<TResults>;
 
   constructor(stmt: Statement) {
-    this.exec = promisify(stmt.exec);
+    // Complicated topic: https://github.com/Microsoft/TypeScript/issues/26048
+    // Since I knew that exec have exactly that signature I used 'as any'.
+    this.exec = promisify(stmt.exec) as any;
   }
 }
 
